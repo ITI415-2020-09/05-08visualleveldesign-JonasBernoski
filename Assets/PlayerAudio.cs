3bb5b6f687bd7ce9ffbc807b22901e1ec2514565
Jonas Bernoski <jonas2015@aol.com>
2020-10-20 19:01:56 -0400

    Audio

---
+++ Assets/PlayerAudio.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class PlayerAudio : MonoBehaviour
+{
+    public AudioClip Footstep01;
+
+    public AudioSource Step;
+
+    public AudioMixerSnapshot IdleSnapshot;
+    public AudioMixerSnapshot SecondSnapshot;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Door"))
+        {
+            Step.PlayOneShot(Footstep01);
+        }
+        if (other.CompareTag("The Zone"))
+        {
+            SecondSnapshot.TransitionTo(0.5f);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Door"))
+        {
+            Step.PlayOneShot(Footstep01);
+        }
+        if (other.CompareTag("The Zone"))
+        {
+            IdleSnapshot.TransitionTo(0.5f);
+        }
+    }
+
+}
