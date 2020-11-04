2900098899a8ea639f31f1a784823004fd6c0018
Jonas Bernoski <jonas2015@aol.com>
2020-11-04 17:53:02 -0500

    Prototype 2 start

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Player : MonoBehaviour
+{
+ 
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+
+    private Rigidbody rb;
+    private int count;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winTextObject.SetActive(false);
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.CompareTag("Ball"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= 20)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+}
